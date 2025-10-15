5284f1047fdbbb8763683531da876ff90e48a80b
Esteban Gonzalez <102267595+Esteban528@users.noreply.github.com>
2025-10-14 22:13:56 -0500

    feat: add yt url to project struct

--- db/projects.go
+++ db/projects.go
@@ -8,25 +8,33 @@ type Project struct {
 	Description string
 	Visit_URL   string
 	Source_URL  string
+	Youtube_URL string
 }
 
 func createProject(p *Project) error {
 	_, err := db.Exec(`INSERT INTO projects 
-						(title, description, visit_url, source_url) VALUES (?,?,?,?)`,
-		p.Title, p.Description, p.Visit_URL, p.Source_URL)
+						(title, description, visit_url, source_url, youtube_url) 
+						VALUES (?,?,?,?,?)`,
+		p.Title, p.Description, p.Visit_URL, p.Source_URL, p.Youtube_URL)
 
 	return err
 }
 
-func FindAllProject() ([]Project) {
+func FindAllProject() []Project {
 	projects := []Project{}
 
-	rows,_ := db.Query("SELECT * FROM projects")
+	rows, _ := db.Query("SELECT * FROM projects")
 
 	for rows.Next() {
 		project := Project{}
-		err := rows.Scan(&project.ID, &project.Title, 
-			&project.Description, &project.Visit_URL, &project.Source_URL)
+		err := rows.Scan(
+			&project.ID,
+			&project.Title,
+			&project.Description,
+			&project.Visit_URL,
+			&project.Source_URL,
+			&project.Youtube_URL,
+		)
 
 		if err != nil {
 			continue
@@ -40,8 +48,14 @@ func FindProject(id int) (Project, error) {
 	project := Project{}
 
 	row := db.QueryRow("SELECT * FROM projects WHERE id = ?", id)
-	err := row.Scan(&project.ID, &project.Title, 
-		&project.Description, &project.Visit_URL, &project.Source_URL)
+	err := row.Scan(
+		&project.ID,
+		&project.Title,
+		&project.Description,
+		&project.Visit_URL,
+		&project.Source_URL,
+		&project.Youtube_URL,
+	)
 
 	return project, err
 }
@@ -52,12 +66,12 @@ func (p *Project) Save() error {
 	}
 
 	_, err := db.Exec(`UPDATE projects 
-		SET title=?, description=?, visit_url=?, source_url=? 
+		SET title=?, description=?, visit_url=?, source_url=?, youtube_url=? 
 		WHERE id=?`,
-		p.Title, p.Description, p.Visit_URL, p.Source_URL, p.ID)
+		p.Title, p.Description, p.Visit_URL, p.Source_URL, p.Youtube_URL, p.ID)
 
 	if err != nil {
-		log.Println("DB error deleting", p, err)
+		log.Println("DB error updating", p, err)
 	}
 	return err
 }
